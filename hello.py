8b1c5ae5a783560c2c5f273f151f16b35eba8610
N-Saikrishna <95255518+N-Saikrishna@users.noreply.github.com>
2026-06-16 23:49:26 -0400

    first program

--- hello.py
+++ hello.py
@@ -1,1 +1,10 @@
+
 print("Hello, world!")
+print ('What is your name?')
+my_name = input('')
+print('it is good to meet you ' + my_name)
+print('the length of your name is: ')
+print(len(my_name))
+print("what is your age?")
+my_age = input('')
+print('you will be ' + str(int(my_age) + 1) + ' in a year.')
